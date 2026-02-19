1faffe00a26c650a081c7337243805a6bdc924f9
Asya0 <anya.gerasimenko763@mail.ru>
2026-02-19 21:17:54 +0600

    info: настройка проекта

---
+++ src/api/recipes.ts
@@ -0,0 +1,19 @@
+import apiClient from './apiClient';
+
+export interface Recipe {
+  id: number;
+  documentId: string;
+  name: string;
+  summary: string;
+  totalTime: number;
+  cookingTime: number;
+  preparationTime: number;
+  servings: number;
+  rating: number;
+  calories: number;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  likes: number;
+  vegetarian: boolean;
+}
